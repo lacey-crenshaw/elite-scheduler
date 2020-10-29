28aaac9b65a423246e237eaf3f9a99d2a69ab28c
lacey-crenshaw <lacey@laceycrenshaw.com>
2020-10-29 07:54:37 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,32 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: '/my-teams',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'game',
+    loadChildren: () => import('./game/game.module').then( m => m.GamePageModule)
+  },
+  {
+    path: 'my-teams',
+    loadChildren: () => import('./my-teams/my-teams.module').then( m => m.MyTeamsPageModule)
+  },
+  {
+    path: 'team-detail',
+    loadChildren: () => import('./team-detail/team-detail.module').then( m => m.TeamDetailPageModule)
+  },
+  {
+    path: 'teams',
+    loadChildren: () => import('./teams/teams.module').then( m => m.TeamsPageModule)
+  },
+  {
+    path: 'tournaments',
+    loadChildren: () => import('./tournaments/tournaments.module').then( m => m.TournamentsPageModule)
   }
 ];
 
---
+++ src/app/my-teams/my-teams.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-teams',
+  templateUrl: './my-teams.page.html',
+  styleUrls: ['./my-teams.page.scss'],
+})
+export class MyTeamsPage implements OnInit {
+
+ goToTournaments() {
+  this.navCtrl.navigateForward('tournaments');
+}
+}
+
